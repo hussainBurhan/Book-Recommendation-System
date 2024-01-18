8bf51a13b7fe3072812d229ea23631170594cd6b
hussainBurhan <144117030+hussainBurhan@users.noreply.github.com>
2024-01-18 19:52:15 +0500

    corrected main file

--- main.py
+++ main.py
@@ -56,8 +56,33 @@ popular_df = popular_df.merge(books, on='Book-Title').drop_duplicates('Book-Titl
 print(popular_df.shape)
 
 # Collaborative Filtering Based
-# (remaining code for collaborative filtering)
-# ...
+users_with_200_ratings = ratings_with_name.groupby('User-ID')['Book-Rating'].count() > 200
+users_with_200_ratings_index = users_with_200_ratings[users_with_200_ratings].index
+filtered_rating = ratings_with_name[ratings_with_name['User-ID'].isin(users_with_200_ratings_index)]
+y = filtered_rating.groupby('Book-Title')['Book-Rating'].count() >= 50
+famous_books = y[y].index
+final_ratings = filtered_rating[filtered_rating['Book-Title'].isin(famous_books)]
+pt = final_ratings.pivot_table(index='Book-Title', columns='User-ID', values='Book-Rating')
+pt.fillna(0, inplace=True)
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+similarity_scores = cosine_similarity(pt)
+print(similarity_scores[0].shape)
+
+def recommend(book_name):
+    index = np.where(pt.index == book_name)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x:x[1], reverse=True)[1:6]
+    data = []
+    for i in similar_items:
+        item = []
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+        data.append(item)
+    return data
 
 # Save dataframes and models to pickle files
 pickle.dump(popular_df, open('popular.pkl', 'wb'))
